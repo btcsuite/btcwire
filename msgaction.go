a45c66a39c3c733125ee86a83969201b3306faba
Christopher Swenson <chris@caswenson.com>
2014-09-06 15:01:53 -0700

    Add timestamp field for action; add tests for serialize / deserialize

--- msgaction.go
+++ msgaction.go
@@ -3,6 +3,7 @@ package btcwire
 import (
 	"fmt"
 	"io"
+	"time"
 )
 
 // MsgAction represents a Gem game action.
@@ -12,20 +13,22 @@ type MsgAction struct {
 	// See MsgActionVersion.
 	Version uint32
 
+	Timestamp time.Time
+
 	// The game this message is for.
 	GameID ShaHash
 
 	// The last message that the author saw before writing this message.
 	// This could be all zeros if it is a game start message.
-	PreviousMessageSignature []byte // could be 0s for game start
+	PreviousMessageSignature [65]byte // could be 0s for game start
 
 	// The player who is writing this message.
-	Author ShaHash
+	Author [25]byte
 
 	// The serialization of this message, signed.
 	// When constructing and verifying this signature, assume these bytes
 	// are set to 0.
-	Signature []byte
+	Signature [65]byte
 
 	// The serialization of the game message.
 	Message []byte
@@ -36,7 +39,7 @@ const (
 	MsgActionVersion = uint32(1)
 
 	// minActionPayload is the minimum payload size for a Gem action.
-	minActionPayload = 4 + 32 + 65 + 25 + 65 + 4 + 1
+	minActionPayload = 4 + 4 + 32 + 65 + 25 + 65 + 4 + 1
 
 	// maxActionMessageLength is set low enough to prevent the length from
 	// being large enough to DoS the receiver.
@@ -55,11 +58,13 @@ func (a *MsgAction) BtcDecode(r io.Reader, pver uint32) error {
 	}
 
 	var dataLen uint32
-	err = readElements(r, &a.GameID, &a.PreviousMessageSignature,
+	var sec uint32
+	err = readElements(r, &sec, &a.GameID, &a.PreviousMessageSignature,
 		&a.Author, &a.Signature, &dataLen)
 	if err != nil {
 		return err
 	}
+	a.Timestamp = time.Unix(int64(sec), 0)
 
 	if dataLen > maxActionMessageLength {
 		return fmt.Errorf("Action message length %d larger than maximum %d",
@@ -82,12 +87,14 @@ func (a *MsgAction) BtcEncode(w io.Writer, pver uint32) error {
 		return fmt.Errorf("Action message length %d larger than maximum %d",
 			len(a.Message), maxActionMessageLength)
 	}
-	err := writeElements(w, a.Version, a.GameID, a.PreviousMessageSignature,
-		a.Author, a.Signature, uint32(len(a.Message)), a.Message)
+	err := writeElements(w, a.Version, uint32(a.Timestamp.Unix()), a.GameID,
+		a.PreviousMessageSignature, a.Author, a.Signature, uint32(len(a.Message)))
 	if err != nil {
 		return err
 	}
-	return nil
+
+	_, err = w.Write(a.Message)
+	return err
 }
 
 // SerializeSize returns the number of bytes it would take to serialize the
